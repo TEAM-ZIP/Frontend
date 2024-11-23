03e27351e3ee3dd5452be1c0c7bfd9db2df0efbf
yongaricode <kang061313@gmail.com>
2024-11-23 17:24:22 +0900

    chore: initial setting

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='text-[40px] font-semibold'>
+        Hello World!
+      </div>
+    </>
+  )
+}
+
+export default App
