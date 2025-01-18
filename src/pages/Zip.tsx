7f980cc9e3d566bc1396e455e4bc504fd5679e22
yongaricode <kang061313@gmail.com>
2025-01-18 14:23:42 +0900

    feat: 카카오 맵 불러오기

--- src/pages/Zip.tsx
+++ src/pages/Zip.tsx
@@ -1,5 +1,23 @@
+import { useEffect } from 'react';
+
+declare global {
+  interface Window {
+    kakao: any;
+  }
+}
+
 const Zip = () => {
-  return <div>Zip</div>;
+  useEffect(() => {
+    let container = document.getElementById(`map`);
+    let options = {
+      center: new window.kakao.maps.LatLng(33.450701, 126.570667),
+      level: 3,
+    };
+
+    let map = new window.kakao.maps.Map(container, options);
+  }, []);
+
+  return <div id="map" className="w-full h-full" />;
 };
 
 export default Zip;
