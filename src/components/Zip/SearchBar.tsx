e0c1a0f015118b6bcea3b894e0c5bb4fded7b7fa
yongaricode <kang061313@gmail.com>
2025-02-23 16:02:01 +0900

    fix: 한글 타이핑 시 중복 입력 오류 수정

--- src/components/Zip/SearchBar.tsx
+++ src/components/Zip/SearchBar.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { CiSearch } from 'react-icons/ci';
 
 interface SearchBarProps {
@@ -6,20 +7,28 @@ interface SearchBarProps {
   onSearch: () => void;
 }
 const SearchBar = ({ setSearchWord, searchWord, onSearch }: SearchBarProps) => {
+  const [isComposing, setIsComposing] = useState(false);
+
   const handleEnter = (e: React.KeyboardEvent) => {
-    if (e.key === 'Enter') {
+    if (!isComposing && e.key === 'Enter') {
       onSearch();
     }
   };
+
   return (
     <label className="relative block">
-      <CiSearch className="text-main_1 absolute left-3 top-1/2 -translate-y-1/2 w-[18px] h-[18px]" />
+      <CiSearch className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-main_1" />
       <input
         placeholder="서점 이름, 지역 검색"
-        className="w-full pl-[46px] text-[14px] py-[10px] bg-white rounded-[12px] focus:outline-none focus:ring-1 focus:ring-main_1"
+        className="w-full rounded-[12px] bg-white py-[10px] pl-[46px] text-[14px] focus:outline-none focus:ring-1 focus:ring-main_1"
         onChange={(e) => setSearchWord(e.target.value)}
         value={searchWord}
         onKeyDown={handleEnter}
+        onCompositionStart={() => setIsComposing(true)}
+        onCompositionEnd={(e) => {
+          setIsComposing(false);
+          setSearchWord(e.currentTarget.value);
+        }}
       />
     </label>
   );
