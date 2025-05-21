17f4a0ef0f82bef44e383979764147943e6a4f55
yongaricode <kang061313@gmail.com>
2025-05-21 16:12:36 +0900

    feat: 서점 해시태그 구현

--- src/components/Zip/TagBar.tsx
+++ src/components/Zip/TagBar.tsx
@@ -1,15 +1,22 @@
+import { useEffect, useState } from 'react';
+import { getHashTag } from '../../api/zip.api';
 import FilterButton from '../../components/Button/FilterButton';
 
 const TagBar = () => {
+  const [tags, setTag] = useState<{ tag: string }[]>([]);
+  useEffect(() => {
+    getHashTag().then((data) => {
+      setTag(data.data);
+    });
+  }, []);
+
   return (
     <div className="relative flex gap-2">
       <div className="mx-[-32px] overflow-x-auto px-[32px] scrollbar-hide">
         <div className="flex gap-[10px] after:w-[20px] after:flex-shrink-0 after:content-['']">
-          <FilterButton text="#고양이" />
-          <FilterButton text="#분위기 있는 서점" />
-          <FilterButton text="#책과 함께 술을" />
-          <FilterButton text="#분위기" />
-          <FilterButton text="#인문서적" />
+          {tags.map((tag, index) => (
+            <FilterButton text={tag.tag} key={index} />
+          ))}
         </div>
       </div>
     </div>
